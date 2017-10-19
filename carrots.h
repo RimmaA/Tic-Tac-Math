5664207b18c85f18726d4bacc071cfafd1a0a0a5
Rimma <axmetova.rimma@gmail.com>
2017-10-19 11:53:02 -0400

    updated files

--- carrots.h
+++ carrots.h
@@ -1,17 +1,17 @@
 /*This file includes all information about beds with carrots(constructor, destructor, functions which checks health of beds, etc.)*/
 
-     	//Rimma Niyazova 
-     	//carrots.h -- Carrots class specification file
-     	#ifndef CARROTS_H
-     	#define CARROTS_H       
-     	class Carrots{
-     	 private:
-     	  int *carrots;
-     	 public:
-    	  Carrots(int, int);
-    	  ~Carrots();
-    	  void checkCell(int, int);
-    	  void checkHealth(int);
-    	  bool finishBeds(int);
-    	};         
-    	#endif
+//Rimma Niyazova 
+//carrots.h -- Carrots class specification file
+#ifndef CARROTS_H
+#define CARROTS_H       
+class Carrots{
+private:
+	int *carrots;
+public:
+	Carrots(int, int);
+	~Carrots();
+	void checkCell(int, int);
+	void checkHealth(int);
+	bool finishBeds(int);
+};         
+#endif
--- field.h
+++ field.h
@@ -1,28 +1,28 @@
 /*This file contains field¿s data such as display fields, who is winner, functions which do manual and random input, etc.*/
-     	//Rimma Niyazova
-     	//field.h -- Field class specification file
-     	#ifndef FIELD_H
-     	#define FIELD_H
-     	#include "carrots.h"       
-     	class Field{
-     	private:
-     	 	char field[64];
-    	 	int location[5];
-    	 	Carrots *bed1, *bed2, *bed3, *bed4, *bed5;
-    	public:
-    	  	Field();
-    	  	~Field();
-    	  	void input();
-    	  	int validPlace(int[], int);
-		void randomInput();
-   	  	int randomPlace(int[], int);
-   	  	void Display();
-   	  	void carrots();
-    	  	void coverDisplay();
-    	  	bool checkBed(int);
-    	  	int validCell(int);
-    	  	int usedCell(int);
-    	  	int finishGame();
-    	  	bool valid(int[], int, int);
-    	};         
-    	#endif
+//Rimma Niyazova
+//field.h -- Field class specification file
+#ifndef FIELD_H
+#define FIELD_H
+#include "carrots.h"       
+class Field{
+private:
+ 	char field[64];
+ 	int location[5];
+ 	Carrots *bed1, *bed2, *bed3, *bed4, *bed5;
+public:
+  	Field();
+  	~Field();
+  	void input();
+  	int validPlace(int[], int);
+	void randomInput();
+   	int randomPlace(int[], int);
+   	void Display();
+   	void carrots();
+    	void coverDisplay();
+    	bool checkBed(int);
+    	int validCell(int);
+    	int usedCell(int);
+    	int finishGame();
+    	bool valid(int[], int, int);
+};         
+#endif
